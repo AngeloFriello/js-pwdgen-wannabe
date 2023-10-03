0a3528ff1f959c3ed13e068a231a0aca02981d4b
angelo friello <angelofriello01@gmail.com>
2023-10-03 15:35:13 +0200

    add variabile cognome

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 
-let message = 'ciao'
-console.log(message);
-
-alert('ciao mondo');
+//# creazione password per l'utente
+//- chiedere il nome all'utente
+//   - dichiarare una variabile
+let userName
+//   - assegnare alla variabile il valore restituito da un prompt
+userName = prompt('Ciao, come ti chiami?')
+console.log(userName)
+//- chiedere il cognome all'utente
+//   - dichiarare una variabile
+let surName
+//   - assegnare alla variabile il valore restituito da un prompt
+surName =prompt('qualè il tuo cognome?')
+//- chiedere il colore preferito all'utente
+//   - dichiarare una variabile
+//   - assegnare alla variabile il valore restituito da un prompt
+//- generare un numero in modo automatico
+//   -generare un numero
+//- creare un messaggio di saluti all'utente
+//   - dichiarare una variabile
+//   - assegnare alla variabile un ciao seguito dal valore restituito dal prompt name
+//-   generare la password all'utente
+//   - unire tutti i valore ricevuti dai prompt per ottenere la pw 
